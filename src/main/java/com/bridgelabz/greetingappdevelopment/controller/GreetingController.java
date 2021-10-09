01b3c38b2f6de770d2fc9dd83849c7fea5e6799b
Megha <meghajagadale99@gmail.com>
2021-10-09 23:26:19 +0530

    Added:Ability for the Greeting App to give Greeting message with name

--- src/main/java/com/bridgelabz/greetingappdevelopment/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingappdevelopment/controller/GreetingController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class GreetingController {
     @Autowired
@@ -16,8 +18,17 @@ public class GreetingController {
         return new ResponseEntity("Welcome to Greeting App", HttpStatus.OK);
     }
 
-    @GetMapping({"/greeting/message"})
+    @GetMapping("/greeting/message")
     public String getWelcomeMessage() {
-        return  greetingService.greetingMessage();
+        return greetingService.greetingMessage();
+    }
+
+    @GetMapping("/greeting/message/{name}")
+    public String greeting(@PathVariable String name) {
+        return "Hello " + name + "! Hello World!";
+    }
+    @GetMapping("/greeting/messages/{name}")
+    public String greeting(@RequestParam(value = "fname") String fname, @RequestParam(value = "lname") String lname) {
+        return "Hello " + fname + " " + lname +"! Hello World!";
     }
 }
